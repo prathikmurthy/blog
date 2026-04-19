78b7ca40505db2d71d0eeb39b53d088575c77605
Prathik M. <murthypr@umich.edu>
2026-04-19 16:56:54 -0400

    add xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://blog.prathikm.dev',
 	integrations: [mdx(), sitemap()]
 });
