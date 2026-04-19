d95da95c768df07186e2d64daaa50632ac61d8a3
Prathik M. <murthypr@umich.edu>
2026-04-19 12:24:51 -0400

    final fixes

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,7 +13,8 @@ const blog = defineCollection({
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
-			tags: z.array(z.string()).optional()
+			tags: z.array(z.string()).optional(),
+			draft: z.boolean().default(false),
 		}),
 });
 
---
+++ src/content/blog/first-post.md
@@ -0,0 +1,12 @@
+---
+title: "first post"
+description: ""
+pubDate: "2026-04-19"
+draft: false
+---
+
+Cool first post, hopefully there's more at some point 
+
+Here's a picture of TWICE I took at their Detroit concert a few weeks ago
+
+![TWICE Concert](./first-post-images/twice.jpeg)
